97b5b5b2c49dfcd16d99f7e7d889e3eddb1a0897
Pandey0809 <56840145+Pandey0809@users.noreply.github.com>
2022-10-31 17:15:05 +0100

    Box plots

--- DataAnalysis/RDataAnalysis.R
+++ DataAnalysis/RDataAnalysis.R
@@ -1,9 +1,20 @@
+#Load data set, These Data set contain aggregated Joule value of each
+# run for both native and web version for Nokia Device
 dataNativeNokia <- read.csv(file = 'nativeNokiaFinal.csv')
 dataWebNokia <- read.csv(file = 'WebNokiaFinal.csv')
 
-#Data value Visualization
+#Data values  
 head(dataNativeNokia)
 head(dataWebNokia)
+
+#We have a extra column X at the starting which needs to be droped
+dataNativeNokia$X<- NULL
+dataWebNokia$X<- NULL
+
+#Data values after dropping X 
+head(dataNativeNokia)
+head(dataWebNokia)
+
 
 #R Auto summary statistics
 summary(dataNativeNokia)
@@ -13,3 +24,11 @@ summary(dataWebNokia)
 
 summary(dataNativeNokia["gmail"])
 summary(dataWebNokia["gmail"])
+
+#boxplot for gmail
+boxplot(dataNativeNokia["gmail"])
+boxplot(dataWebNokia["gmail"])
+
+#boxplot for full datasets
+ggplot(stack(dataNativeNokia), aes(x = ind, y = values)) +     geom_boxplot()
+ggplot(stack(dataWebNokia), aes(x = ind, y = values)) +    geom_boxplot()
